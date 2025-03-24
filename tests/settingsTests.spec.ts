99b0e1ebede4d3e36163169f5787b39883aab1d4
RoaldMRasmussen <94868948+RoaldMRasmussen@users.noreply.github.com>
2025-03-24 13:30:48 -0400

    refactor: improve formatting and naming in settings tests

--- tests/settingsTests.spec.ts
+++ tests/settingsTests.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
 const { v4: uuidv4 } = require('uuid');
 
-const adminSites = ['admin4.xyvid.com', 'admin6.xyvid.com', 'admin8.xyvid.com', 'admin11.xyvid.com'];
+const adminSites = [
+  'admin4.xyvid.com', 
+  'admin6.xyvid.com', 
+  'admin8.xyvid.com', 
+  'admin11.xyvid.com'
+];
 
 for (const site of adminSites) {
   test.describe(`Event Settings ${site}`, () => {
     test.beforeEach(async ({ page }) => {
       test.setTimeout(60000);
-      //Log in, navigate to the calendar and click on the New Event button
+      // Log in, navigate to the calendar and click on the New Event button
       await page.goto(`https://${site}`);
       await page.getByRole('textbox', { name: 'Username' }).fill('mrasmussen@xyvid.com');
       await page.getByRole('textbox', { name: 'Password' }).fill('@HaleyAnnika1');
@@ -18,27 +23,28 @@ for (const site of adminSites) {
     });
 
     test('EventName', async ({ page }) => {
-      //Enter event name and test alert for empty folder directory
-      await page.getByRole('textbox', { name: 'Event Name' }).fill(`Mytest${uuidv4()}`);
+      // Enter event name and test alert for empty folder directory
+      await page.getByRole('textbox', { name: 'Event Name' }).fill(`RMRtest${uuidv4()}`);
       await page.getByText('Create The settings above').click();
       await expect(page.locator('text=The event folder directory is invalid')).toBeVisible();
     });
 
     test('SelectFolder', async ({ page }) => {
-      //  Select a folder and test alert for empty vanity URL
+      // Select a folder and test alert for empty vanity URL
       await page.getByRole('button', { name: '' }).click();
       await page.getByRole('treeitem', { name: /test/i }).locator('div').first().click();
       await page.getByText('Select This FolderClick Here').click();
       await page.getByText('Create The settings above').click();
       await expect(page.locator('text=The vanity url is required')).toBeVisible();
     });
+
     test('VanityURL', async ({ page }) => {
       // Fill in the Event Name and Vanity URL
-      await page.getByRole('textbox', { name: 'Event Name' }).fill(`Mytest${uuidv4()}`);
+      await page.getByRole('textbox', { name: 'Event Name' }).fill(`RMRtest${uuidv4()}`);
       await page.getByRole('button', { name: '' }).click();
 
       // Trim the eventName to 30 characters for vanity URL
-      const eventName = `Mytest${uuidv4()}`;
+      const eventName = `RMRtest${uuidv4()}`;
       const trimmedVanityURL = eventName.slice(0, 30);
     
       // Fill the vanity URL field with the trimmed value
@@ -48,22 +54,25 @@ for (const site of adminSites) {
       // Verify the trimmed length of vanity URL
       const vanityURLValue = await page.locator('#txt_vanityURL').inputValue();
       expect(vanityURLValue.length).toBe(30);        
-    });  
-    test('SelectDates', async ({ page }) => {
-        await page.locator('#txt_startDateGroup span').first().click();
-        await page.locator('.day.active.today').click();
-        await page.locator('#txt_startDate').press('Enter');
-        await page.locator('#txt_openTimeGroup span').nth(1).click();
-        await page.locator('#txt_openTimeGroup > .input-group-addon').click();
-        await page.locator('#txt_startTimeGroup span').nth(1).click();
-        await page.getByRole('link', { name: ' Increment Minutes' }).click();
-        await page.locator('#txt_startTimeGroup > .input-group-addon > .glyphicon').click();
-        await page.locator('#txt_endTime').click();
-        await page.locator('#txt_endTimeGroup span').nth(1).click();
-        await page.getByRole('link', { name: ' Increment Hour' }).click();
-        await page.locator('#txt_endTimeGroup > .input-group-addon > .glyphicon').click();
+    });
 
-    });  
+    test('SelectDates', async ({ page }) => {
+      await page.locator('#txt_startDateGroup span').first().click();
+      await page.locator('.day.active.today').click();
+      await page.locator('#txt_startDate').press('Enter');
+      await page.locator('#txt_openTimeGroup span').nth(1).click();
+      await page.locator('#txt_openTimeGroup > .input-group-addon').click();
+      await page.locator('#txt_startTimeGroup span').nth(1).click();
+      await page.getByRole('link', { name: ' Increment Minutes' }).click();
+      await page.locator('#txt_startTimeGroup > .input-group-addon > .glyphicon').click();
+      await page.locator('#txt_endTime').click();
+      await page.locator('#txt_endTimeGroup span').nth(1).click();
+      await page.getByRole('link', { name: ' Increment Hour' }).click();
+      await page.locator('#txt_endTimeGroup > .input-group-addon > .glyphicon').click();
+    });
+    test('TurnHybridOn', async ({ page }) => {
+      await page.locator('#cmb_hybrid').selectOption('1');
+    }); 
 
   });
-} 
+}
